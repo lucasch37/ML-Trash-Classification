cb5f57d2f339cc50294b2e5df7ede0b080096a4a
Lucas Chen <130620394+lucasch37@users.noreply.github.com>
2024-04-23 23:55:16 -0700

    fixed file paths and updated readme

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ def predict():
         filename = secure_filename(file.filename)
         file.save(
             os.path.join(
-                "/Users/lucas/Documents/vscode/waste-classification/backend/static",
+                "backend/static",
                 filename,
             )
         )
@@ -29,7 +29,7 @@ def predict():
 
 
 def getPrediction(filename):
-    model_path = "/Users/lucas/Documents/vscode/waste-classification/backend/model/optimized_model.tflite"
+    model_path = "backend/model/optimized_model.tflite"
 
     # Load the model using TensorFlow Lite interpreter
     interpreter = tf.lite.Interpreter(model_path=model_path)
@@ -37,7 +37,7 @@ def getPrediction(filename):
 
     # Load and preprocess the image
     img = load_img(
-        "/Users/lucas/Documents/vscode/waste-classification/backend/static/" + filename,
+        "backend/static/" + filename,
         target_size=(180, 180),
     )
     img = img_to_array(img) / 255.0
@@ -73,7 +73,7 @@ def getPrediction(filename):
     probability_results = str(probability_results)
     os.remove(
         os.path.join(
-            "/Users/lucas/Documents/vscode/waste-classification/backend/static/",
+            "backend/static/",
             filename,
         )
     )
